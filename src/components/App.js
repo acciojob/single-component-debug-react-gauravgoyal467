cfc59b56dfebc511b316a1fe25aec838538eb024
GAURAV GOYAL <116831689+gauravgoyal467@users.noreply.github.com>
2023-07-17 08:14:54 +0000

    2nd try

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,7 @@ import React from "react"
 
 const App=()=>{
     return (
-        <div>
-            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
-        </div>
+     <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
     )
 }
 
